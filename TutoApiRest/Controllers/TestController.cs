d80a4f6ee1a87f9f691824069d72b57915112880
TaniaBar <tania.barillaro1@gmail.com>
2025-08-05 13:21:04 +0200

    Création controller et dossier dto

---
+++ TutoApiRest/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TutoApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<TestResponse> TestGet()
+        {
+        }
+
+        [HttpPost]
+        public ActionResult<TestResponse> TestPost(TestRequest request)
+        {
+            
+        }
+    }
+}
